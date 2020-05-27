b63e6446e0976bc4913f502feba9209b4bfd7498
Thom van den Hork <thomvandenhork@gmail.com>
2020-05-27 08:11:08 +0200

    Update email_collect.rb

--- app/services/message_templates/template/email_collect.rb
+++ app/services/message_templates/template/email_collect.rb
@@ -5,7 +5,7 @@ class MessageTemplates::Template::EmailCollect
     ActiveRecord::Base.transaction do
       conversation.messages.create!(typical_reply_message_params)
       # conversation.messages.create!(ways_to_reach_you_message_params)
-      # conversation.messages.create!(email_input_box_template_message_params)
+      conversation.messages.create!(email_input_box_template_message_params)
     end
   rescue StandardError => e
     Raven.capture_exception(e)
